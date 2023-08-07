04cc9815e31046437f0f7f99ea004e57f4c07cae
Roy Kolak <roy.kolak@gmail.com>
2023-08-07 11:37:39 -0500

    built package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,12 @@
 /// <reference types="react" />
 import { Dimensions, MacroConfig, MacroName, Pixel } from "display-engine/lib/types";
 export declare function updateDot({ y, x, hex }: Pixel): void;
+type MacroNameType = `${MacroName}`;
 export declare function BigdotsClient({ config, }: {
     config: {
-        macroName: MacroName;
-        macroConfig: Partial<MacroConfig>;
+        macroName?: MacroNameType;
+        macroConfig?: Partial<MacroConfig>;
         dimensions: Dimensions;
     };
 }): JSX.Element;
+export {};

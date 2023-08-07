fa80359fbc8a858a8ae2f2d466975385490ebd5f
Roy Kolak <roy.kolak@gmail.com>
2023-08-07 10:44:57 -0500

    slim repo again

--- lib/display.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import { Dimensions, MacroConfig, MacroName, Pixel } from "display-engine/lib/types";
-export declare function updateDot({ y, x, hex }: Pixel): void;
-export declare function BigdotsClient(config: {
-    macroName: MacroName;
-    macroConfig: Partial<MacroConfig>;
-    dimensions: Dimensions;
-}): JSX.Element;

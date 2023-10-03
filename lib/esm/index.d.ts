78df09d7cc00514c452adc15df9cc260f7493541
Roy Kolak <roykolak@fastmail.com>
2023-10-03 17:23:36 -0500

    Expose new image macro config

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react" />
 import { Dimensions, Macro, MacroConfig, MacroName, Pixel } from "@bigdots-io/display-engine/lib/esm/types";
-export { solidColor, text, twinkle, meteorShower, marquee, } from "@bigdots-io/display-engine";
+export { solidColor, text, twinkle, meteorShower, marquee, image } from "@bigdots-io/display-engine";
 export declare function updateDot(element: HTMLDivElement, { y, x, hex, brightness }: Pixel): void;
 type MacroNameType = `${MacroName}`;
 export default function BigdotsDisplay({ layers, dimensions, }: {

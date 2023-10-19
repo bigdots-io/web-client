4cc783f95b71b9386073e3998378532f7ee88b81
Roy Kolak <roykolak@fastmail.com>
2023-10-19 08:13:10 -0500

    integrate stop support

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -3,9 +3,10 @@ import { Dimensions, Macro, MacroConfig, MacroName, Pixel } from "@bigdots-io/di
 export { solidColor, text, twinkle, meteorShower, marquee, image, time, } from "@bigdots-io/display-engine";
 export declare function updateDot(element: HTMLDivElement, { y, x, hex, brightness }: Pixel): void;
 type MacroNameType = `${MacroName}`;
-export default function BigdotsDisplay({ layers, dimensions, }: {
+export default function BigdotsDisplay({ layers, dimensions, devMode, }: {
     layers: Macro[];
     macroName?: MacroNameType;
     macroConfig?: Partial<MacroConfig>;
     dimensions: Dimensions;
+    devMode?: boolean;
 }): JSX.Element;
